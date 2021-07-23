9f4c8cd95fee9f59329a74f8bdf5577409cfb84b
Brendan O'Brien <brendan.o'brien@iress.com>
2021-07-23 21:30:23 +1000

    added ticket belongs_to user association

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative './data_library'
+
 # ticket class
 class Ticket
   attr_reader(:id, :created_at, :type, :subject, :assignee_id, :tags)
@@ -11,5 +13,9 @@ class Ticket
     @subject = subject
     @assignee_id = assignee_id
     @tags = tags
+  end
+
+  def user
+    DataLibrary.users.select { |user| user.id == assignee_id }.first
   end
 end
--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -4,23 +4,45 @@ require 'rspec'
 require_relative '../lib/ticket'
 
 RSpec.describe Ticket do
-  let(:ticket) do
-    Ticket.new(id: 'b07a8c20-2ee5-493b-9ebf-f6321b95966e', created_at: '2016-03-21T11:18:13-11:00', type: 'question',
-               subject: 'a cold day in Melbourne', assignee_id: 24, tags: ['victoria', 'new south wales'])
-  end
-
-  let(:unassigned_ticket) do
-    Ticket.new(id: 'b07a8c20-2ee5-493b-9ebf-f6321b95966e', created_at: '2016-03-21T11:18:13-11:00', type: 'question',
-               subject: 'a cold day in Melbourne', tags: ['victoria', 'new south wales'])
-  end
-
   describe '#initialize' do
+    let(:ticket) do
+      Ticket.new(id: 'b07a8c20-2ee5-493b-9ebf-f6321b95966e', created_at: '2016-03-21T11:18:13-11:00', type: 'question',
+                 subject: 'a cold day in Melbourne', assignee_id: 24, tags: ['victoria', 'new south wales'])
+    end
+
+    let(:unassigned_ticket) do
+      Ticket.new(id: 'b07a8c20-2ee5-493b-9ebf-f6321b95966e', created_at: '2016-03-21T11:18:13-11:00', type: 'question',
+                 subject: 'a cold day in Melbourne', tags: ['victoria', 'new south wales'])
+    end
+
     it 'sets the assignee_id to the given value' do
       expect(ticket.assignee_id).to be(24)
     end
 
     it 'sets the assignee_id to the default value' do
       expect(unassigned_ticket.assignee_id).to be(-1)
+    end
+  end
+
+  describe '#user' do
+    let(:ticket) do
+      Ticket.new(
+        id: '436bf9b0-1147-4c0a-8439-6f79833bff5b',
+        created_at: '2016-04-28T11:19:34-10:00',
+        type: 'incident',
+        subject: 'A Catastrophe in Korea (North)',
+        assignee_id: 24,
+        tags: [
+          'Ohio',
+          'Pennsylvania',
+          'American Samoa',
+          'Northern Mariana Islands'
+        ]
+      )
+    end
+
+    it 'returns the user assigned to the ticket' do
+      expect(ticket.user.name).to eq('Harris Côpeland')
     end
   end
 end
