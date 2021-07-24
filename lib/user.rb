7bf990c70214250b3f2f5f62f4a00e6d164fd3c8
Brendan O'Brien <brendan.o'brien@iress.com>
2021-07-24 14:32:02 +1000

    minor cleanup of user to be DRY

--- lib/user.rb
+++ lib/user.rb
@@ -6,14 +6,14 @@ require_relative './data_library'
 class User
   attr_reader(:id, :name, :created_at, :verified)
 
+  def self.all
+    DataLibrary.users
+  end
+
   def self.search(search_term:, search_value:)
     # TODO: handle case where search term doesn't exist
     # TODO: handle case gracefully where no users are returned
-    DataLibrary.users.select { |user| user.public_send(search_term) == search_value.to_s }
-  end
-
-  def self.all
-    DataLibrary.users
+    self.all.select { |user| user.public_send(search_term) == search_value.to_s }
   end
 
   def initialize(id:, name:, created_at:, verified:)
